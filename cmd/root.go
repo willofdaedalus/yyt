35b01c6778b7a5c25b1167b9062093dd96e74ccc
ccoVeille <3875889+ccoVeille@users.noreply.github.com>
2024-05-06 14:05:32 +0200

    Remove dead code
    This code seems to have been copy pasted from a tutorial.
    It has no use.

--- cmd/root.go
+++ cmd/root.go
@@ -19,25 +19,11 @@ var rootCmd = &cobra.Command{
 	Long: `yyt is a clipboard-like tool that allows you to keep track of files
 that you wish to copy, move or delete later. It is useful when you are working
 on the commandline and you want to keep track of files that you want to work.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() error {
+	rootCmd.SetErrPrefix("yyt: error")
 	return rootCmd.Execute()
-}
-
-func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.yyt.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
